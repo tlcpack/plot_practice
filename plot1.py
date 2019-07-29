073fa5a305695d7124a579bd9ccd4fff7f5cd9a2
Taylor Cooke <taylorcooke@gmail.com>
2019-07-29 10:48:29 -0400

    plots are showing

--- plot1.py
+++ plot1.py
@@ -3,6 +3,20 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 2.0*np.pi, 101)
 y = np.sin(x)
+z = np.cos(x)
 
-plt.plot(x,y)
+xnumbers = np.linspace(0, 7, 15)
+ynumbers = np.linspace(-1, 1, 11)
+
+plt.plot(x, y, color='r', label='sin')
+plt.plot(x, z, color='g', label='cos')
+plt.xlabel('Angle in Radians')
+plt.ylabel('Magnitude')
+plt.title('Trig functions')
+plt.xticks(xnumbers)
+plt.yticks(ynumbers)
+plt.legend()
+plt.grid()
+plt.axis([0, 6.5, -1.1, 1.1])
+
 plt.show()
